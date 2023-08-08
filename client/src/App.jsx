242649f9332f29b5a4de893ec990f9b3c2b46200
Topman-14 <talktotimothy101@gmail.com>
2023-08-08 16:13:09 +0100

    add frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+          <div className="pages">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <h2>Home</h2>
+    </div>
+  )
+}
