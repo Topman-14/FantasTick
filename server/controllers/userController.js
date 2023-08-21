6f22aec0e5cb20e88887f9a0872defae2b9b32b8
Topman-14 <talktotimothy101@gmail.com>
2023-08-21 11:46:20 +0100

    add login and signup functions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,37 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id, username) => {
+    return jwt.sign({_id, username}, process.env.SECRET, { expiresIn: '3d'})
+}
 
 //Login
 const loginUser = async (req, res) =>{
-    res.json({mssg: 'login user'})
+   const {email, password} = req.body
+   try{
+    const user = await User.login(email, password)
+
+    const username = user.username
+
+    const token = createToken(user._id, username)
+
+    res.status(200).json({email, username, token})
+    } 
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 //Signup
 const signupUser = async (req, res) =>{
-    const { email, password } = req.body
+    const { username, email, password } = req.body
 
     try{
-        const user = await User.signup(email, password)
+        const user = await User.signup(username, email, password)
 
-        res.status(200).json({email, user})
+        const token = createToken(user._id, user.username)
+
+        res.status(200).json({email, username, token})
     } catch(error){
         res.status(400).json({error: error.message})
     }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,6 +5,10 @@ const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true
+    },
     email:{
         type: String,
         required: true,
@@ -16,9 +20,20 @@ const userSchema = new Schema({
     }
 })
 
-userSchema.statics.signup = async function (email, password) {
+userSchema.statics.signup = async function (username, email, password) {
 
-    
+    if(!email || !password || !username){
+        throw Error("All inputs must be filled")
+    }
+    if (!validator.isAlphanumeric(username)) {
+        throw Error("Invalid userName. It should only contain letters and numbers.")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Email not valid")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password not strong enough")
+    }
 
     const exists = await this.findOne({ email })
 
@@ -29,10 +44,28 @@ userSchema.statics.signup = async function (email, password) {
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
-    const user = await this.create({ email, password: hash})
+    const user = await this.create({ username, email, password: hash})
 
     return user
 
+}
+
+userSchema.statics.login = async function(email, password){
+    if(!email || !password ){
+        throw Error("All inputs must be filled")
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user) {
+        throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){ throw Error('Incorrect password')}
+
+    return user
 }
 
 module.exports = mongoose.model('User', userSchema)
