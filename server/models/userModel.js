060d27196c83c3fa19b221e016c926daebe90aa9
Topman-14 <talktotimothy101@gmail.com>
2023-08-21 09:21:49 +0100

    update user model static function

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,24 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+userSchema.statics.signup = async function (email, password) {
+
+    
+
+    const exists = await this.findOne({ email })
+
+    if(exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash})
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
